df519478271b3ec5f43d8fa5bf0cbc1efaa243b8
DanieleSpuria <daniele.spuria@proton.me>
2023-10-04 15:48:29 +0200

    add Pizza

---
+++ src/main/java/org/java/app/pojo/Pizza.java
@@ -0,0 +1,76 @@
+package org.java.app.pojo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Pizza {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(length = 128, nullable = false)
+	private String nome;
+	private String descrizione;
+	private String foto;
+	
+	@Column(nullable = false) 
+	private float prezzo;
+	
+	public Pizza(String nome, String descrizione, String foto) {
+		setNome(nome);
+		setDescrizione(descrizione);
+		setFoto(foto);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public float getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(float prezzo) {
+		this.prezzo = prezzo;
+	}
+	
+	@Override
+	public String toString() {
+		return "[ " + getId() + " ] : " 
+				+ getNome() + " : "
+				+ getPrezzo();
+	}
+}
