c0521a559d699c62caec06120599c30cc96f3213
DanieleSpuria <daniele.spuria@proton.me>
2023-10-10 13:13:44 +0200

    add edit-update

--- src/main/java/org/java/app/mvc/controller/PizzaController.java
+++ src/main/java/org/java/app/mvc/controller/PizzaController.java
@@ -49,24 +49,48 @@ public class PizzaController {
 		return "show";
 	}
 	
-	@GetMapping("/new-pizza")
+	@GetMapping("/create-update")
 	public String create(Model model) {
 		
 		model.addAttribute("pizza", new Pizza());
 		
-		return "new-pizza";
+		return "create-update";
 	}
 	
-	@PostMapping("/new-pizza")
+	@PostMapping("/create-update")
 	public String store(
 			@Valid @ModelAttribute("pizza") Pizza formPizza, 
 			BindingResult bindingResult,
 			Model model) {
 		
 		if (bindingResult.hasErrors()) 
-			return "/new-pizza";
+			return "create-update";
 		
 		pizzaRepo.save(formPizza);
+		
+		return "redirect:/";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String edit(
+			@PathVariable Integer id,
+			Model model) {
+		
+		model.addAttribute("pizza", pizzaService.findById(id));
+		
+		return "create-update";
+	}
+	
+	@PostMapping("/edit/{id}")
+	public String update(
+			@Valid @ModelAttribute("pizza") Pizza formPizza,
+			BindingResult bindingResult,
+			Model model) {
+		
+		if (bindingResult.hasErrors()) 
+			return "create-update";
+		
+		pizzaService.save(formPizza);
 		
 		return "redirect:/";
 	}
